57c6df6bdfc09088db6e105d363e5143160cff39
Alistair Mackay <alistair.mackay.work@gmail.com>
2016-07-01 16:59:16 +0100

    Created seed data for database

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
+//Mongo Database require variables
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/comment_application';
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+app.get('/api/characters', function(req, res) {
+	MongoClient.connect(url, function(err, db) {
+		var collection = db.collection("characters");
+		collection.find({}).toArray(function(err, docs) {
+			res.json(docs);
+			db.close();
+		});
+	});
 });
 
 app.use(express.static("client/build"));
@@ -14,3 +28,5 @@ var server = app.listen(3000, function () {
 
   console.log("Example app listening at http://%s:%s", host, port);
 });
+
+// FIXME: Need to require in mongodb from NPM to run and parse the information from our database.
